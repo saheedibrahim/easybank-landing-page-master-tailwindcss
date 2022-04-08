4f04583f33242143e24023050d3cdaef9384bdb9
saheedibrahim <saolabram@gmail.com>
2022-04-08 09:32:32 +0100

    first project with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color:{
+        footer: '#2D314E',
+        'latest-article': '#FAFAFA'
+      },
+      height: {
+        '800': '47.8rem',
+        '100': '30rem'
+      },
+      
+    backgroundPosition: {
+      'testing-ne': 'right -500px'
+    },
+      borderWidth: {
+        '3': '3px',
+      },
+      margin: {
+        '600': '600px',
+      }
+    },
+  },
+  plugins: [],
+}
